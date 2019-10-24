d3dbc2ab69b2d843e836c753add91206f981590a
birkborkan <jirard5@gmail.com>
2019-10-23 19:01:15 -0700

    after add clock calender2

--- data/darfour.sql
+++ data/darfour.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 19, 2019 at 12:36 PM
+-- Generation Time: Oct 24, 2019 at 02:00 AM
 -- Server version: 5.7.13-log
 -- PHP Version: 5.6.22
 
@@ -23,6 +23,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` int(11) NOT NULL,
+  `class_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `classes`
+--
+
+INSERT INTO `classes` (`id`, `class_name`) VALUES
+(1, 'الاول'),
+(2, 'الثاني'),
+(4, 'الثاث'),
+(5, 'الرابع'),
+(6, 'الخامس'),
+(7, 'السادس'),
+(8, 'السابع'),
+(9, 'الثامن'),
+(10, 'التاسع'),
+(11, 'العاشر'),
+(12, 'الحادي عشر'),
+(13, 'الثاني عشر');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `depart_table`
 --
 
@@ -32,8 +61,21 @@ CREATE TABLE `depart_table` (
   `depart_name` varchar(255) NOT NULL,
   `depart_asign` varchar(255) NOT NULL,
   `status` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sem_no` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `depart_table`
+--
+
+INSERT INTO `depart_table` (`id`, `fac_name`, `depart_name`, `depart_asign`, `status`, `date`, `sem_no`) VALUES
+(58, 'الطب', 'الجراحة', 'jraha', 1, '2019-10-22 17:05:03', '12'),
+(59, 'الطب', 'البيطرة', 'bit', 0, '2019-10-22 17:05:37', '12'),
+(61, 'الطب', 'y', '3', 1, '2019-10-22 17:25:50', '3'),
+(62, 'الطب', 'y4', '34', 1, '2019-10-22 17:26:19', '3'),
+(63, 'الطب', '5', '5', 0, '2019-10-23 07:58:05', '5'),
+(64, 'الطب', '55', '5', 1, '2019-10-23 07:58:17', '5');
 
 -- --------------------------------------------------------
 
@@ -55,7 +97,9 @@ INSERT INTO `fac_table` (`id`, `fac_name`, `date`) VALUES
 (4, 'الطب', '2019-10-19 12:02:58'),
 (5, 'التقانة', '2019-10-19 12:07:15'),
 (6, 'الزراعة', '2019-10-19 12:07:24'),
-(7, '222', '2019-10-19 12:19:47');
+(7, '222', '2019-10-19 12:19:47'),
+(8, 'هندسه', '2019-10-19 17:17:53'),
+(9, 'computer cs', '2019-10-21 19:14:10');
 
 -- --------------------------------------------------------
 
@@ -83,6 +127,12 @@ INSERT INTO `login` (`id`, `username`, `password`, `name`, `status`) VALUES
 --
 
 --
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `depart_table`
 --
 ALTER TABLE `depart_table`
@@ -105,15 +155,20 @@ ALTER TABLE `login`
 --
 
 --
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
 -- AUTO_INCREMENT for table `depart_table`
 --
 ALTER TABLE `depart_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 --
 -- AUTO_INCREMENT for table `fac_table`
 --
 ALTER TABLE `fac_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `login`
 --
